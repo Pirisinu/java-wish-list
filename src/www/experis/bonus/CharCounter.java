6e9519eb0c34df86f3a0c5b792188f6ef0c5e197
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 15:12:00 +0200

    added loop to check letters

--- src/www/experis/bonus/CharCounter.java
+++ src/www/experis/bonus/CharCounter.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class CharCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci la parola");
         String inputString = scanner.nextLine();
+        char[] inputToCharArray = inputString.toCharArray();
+        for (int i = 0, j = inputString.length() - 1; i < inputString.length(); i++ ,j--) {
+            System.out.println("i = " + inputToCharArray[i]);
+            System.out.println("j = " + inputToCharArray[j]);
+        }
     }
 }
