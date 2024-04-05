972742a5e9630367dd600067b428000701ad1b95
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 14:03:03 +0200

    add Gift class

---
+++ src/www/experis/wishList/Gift.java
@@ -0,0 +1,20 @@
+package www.experis.wishList;
+
+public class Gift {
+    // Attributes
+    private String name;
+    // Constructor
+
+    public Gift(String name) {
+        this.name = name;
+    }
+
+    // Getter e Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
