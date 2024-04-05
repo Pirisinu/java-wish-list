96b312b18d3f991c5071b2d65b25e0a21bf1f016
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 14:14:09 +0200

    add do-while in main

--- src/www/experis/wishList/Main.java
+++ src/www/experis/wishList/Main.java
@@ -2,14 +2,30 @@ package www.experis.wishList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to you're wish list.");
         boolean exit = false;
         List<Gift> wishList = new ArrayList<Gift>();
+        Scanner scan = new Scanner(System.in);
 
         do {
+            System.out.println("Do you want to include a gift? Y/N:");
+            String choice = scan.nextLine();
+            switch (choice){
+                case "y":
+
+                    break;
+                case "N":
+                    exit = true
+                    break;
+                default:
+                    System.out.println("Choise not valid.");
+                    break;
+            }
+
             System.out.println(wishList.size());
             exit = true;
 
