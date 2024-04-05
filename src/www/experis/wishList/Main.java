b923a9645e8fbf692aee999aa4b778b79ad53547
AlessandroPirisinu <palazzialessandro2@gmail.com>
2024-04-05 14:08:15 +0200

    add Arraylist wishlist + test size()

--- src/www/experis/wishList/Main.java
+++ src/www/experis/wishList/Main.java
@@ -1,7 +1,18 @@
 package www.experis.wishList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to you're wish list.");
+        boolean exit = false;
+        List<Gift> wishList = new ArrayList<Gift>();
+
+        do {
+            System.out.println(wishList.size());
+            exit = true;
+
+        } while (!exit);
     }
 }
